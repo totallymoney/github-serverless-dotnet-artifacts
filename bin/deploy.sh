c8a6326be5480da1d34acba5f0f29d48aca1535c
Blair55 <blair.nicholas@googlemail.com>
2019-07-23 17:03:04 +0100

    Init

---
+++ bin/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 3 ]; then
+    echo "$0": usage: deploy.sh REPOSITORY VERSION ENVIRONMENT
+    echo "$0": eg: deploy.sh mediaingenuity/myrepo 1.2.3456 stage
+    exit 1
+fi
+
+if [ -z "$GITHUB_OAUTH_TOKEN" ]; then
+    echo "Need to set GITHUB_OAUTH_TOKEN"
+    exit 1
+fi
+
+REPOSITORY=$1
+export VERSION=$2
+ENVIRONMENT=$3
+RELEASES_API="https://api.github.com/repos/$REPOSITORY/releases"
+ASSET_ID=$(curl -s -H "Authorization: token $GITHUB_OAUTH_TOKEN" "$RELEASES_API/tags/$VERSION" | jq ".assets[0].id")
+DEPLOY_DIR="deploy"
+DEPLOY_ZIP="$DEPLOY_DIR.zip"
+
+curl -sL -H "Accept: application/octet-stream" "$RELEASES_API/assets/$ASSET_ID?access_token=$GITHUB_OAUTH_TOKEN" -o "$DEPLOY_ZIP"
+rm -rf $DEPLOY_DIR
+unzip $DEPLOY_ZIP -d $DEPLOY_DIR
+rm -rf $DEPLOY_ZIP
+cd $DEPLOY_DIR
+yarn install -s --no-progress
+yarn run sls deploy --stage "$ENVIRONMENT" --force --verbose
+cd .. || exit
---
+++ bin/publish.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 3 ]; then
+    echo "$0": usage: publish.sh REPOSITORY VERSION COMMIT
+    echo "$0": eg: publish.sh mediaingenuity/myrepo 1.2.3456 master
+    echo "$0": OR
+    echo "$0": eg: publish.sh mediaingenuity/myrepo 1.2.3456 b7232ef30b17afbe6d6b4703784cd5e69956e104
+    exit 1
+fi
+
+if [ -z "$GITHUB_OAUTH_TOKEN" ]; then
+    echo "Need to set GITHUB_OAUTH_TOKEN"
+    exit 1
+fi
+
+REPOSITORY=$1
+VERSION=$2
+COMMIT=$3
+PUBLISH_DIR="publish"
+PUBLISH_ZIP="$VERSION.zip"
+
+rm -rf $PUBLISH_DIR
+mkdir $PUBLISH_DIR
+dotnet lambda package "$PUBLISH_DIR/package.zip" -pl src -f netcoreapp2.1 -c Release
+cp serverless.yml "$PUBLISH_DIR/serverless.yml"
+cp package.json "$PUBLISH_DIR/package.json"
+cp yarn.lock "$PUBLISH_DIR/yarn.lock"
+cd $PUBLISH_DIR
+zip -r "$PUBLISH_ZIP" .
+cd .. || exit
+
+git fetch origin
+LATEST_TAG="$(git describe --tags --abbrev=0)"
+MSGS_SINCE_LATEST_TAG="$(git shortlog "$LATEST_TAG".."$COMMIT" --pretty="%h %s")"
+
+jq -n \
+    --arg name "$VERSION" \
+    --arg commit "$COMMIT" \
+    --arg body "$MSGS_SINCE_LATEST_TAG" \
+    '{ tag_name: $name, name: $name, target_commitish: $commit, body: $body }' |
+    curl -s -X POST -d@- "https://api.github.com/repos/$REPOSITORY/releases?access_token=$GITHUB_OAUTH_TOKEN" |
+    jq ".upload_url" |
+    sed "s/{?name,label}/?name=$PUBLISH_ZIP/" |
+    sed 's/"//g' |
+    xargs curl -s -X POST -H "Authorization: token $GITHUB_OAUTH_TOKEN" -H "Content-Type: application/octet-stream" --data-binary @"$PUBLISH_DIR/$PUBLISH_ZIP" |
+    jq
