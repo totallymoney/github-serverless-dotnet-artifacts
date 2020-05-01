6da6519ac4e3351b604a89cf5a236a9787bc9e89
Blair55 <blair.nicholas@googlemail.com>
2020-05-01 14:19:00 +0100

    Refactor config vars

--- bin/picker.py
+++ bin/picker.py
@@ -3,6 +3,9 @@ from signal import signal, SIGINT
 import subprocess
 import pick
 
+max_versions = 5
+env_options = ['stage', 'prod']
+
 
 def handler(signal_received, frame):
     print('SIGINT or CTRL-C detected. Exiting gracefully')
@@ -10,13 +13,13 @@ def handler(signal_received, frame):
 
 
 if __name__ == '__main__':
-    # Tell Python to run the handler() function when SIGINT is recieved
     signal(SIGINT, handler)
     subprocess.call(['git', 'fetch', 'origin'])
-    tags_cmd = 'git tag -l -n --sort=-v:refname "*.*" | head -5'
+    tags_cmd = 'git tag -l -n --sort=-v:refname "*.*" | head -{}'.format(
+        max_versions)
     tags_str = subprocess.check_output(tags_cmd, shell=True)
     tag_msg, _ = pick.pick(tags_str.splitlines(), 'choose version')
-    env, _ = pick.pick(['stage', 'prod'], 'choose environment')
+    env, _ = pick.pick(env_options, 'choose environment')
     version = tag_msg.split()[0]
     confirm_msg = 'deploy {} to {}'.format(version, env)
     _, confirm_res = pick.pick([confirm_msg, 'cancel'], 'confirm')
@@ -24,4 +27,4 @@ if __name__ == '__main__':
         print(confirm_msg)
         subprocess.call(["yarn", "deploy", version, env])
     else:
-        print('cancelled')
+        print('cancelled!')
