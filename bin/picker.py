4b54e05cba138e4edfecc1c01c79762c9b543bf9
Blair55 <blair.nicholas@googlemail.com>
2020-05-01 11:07:34 +0100

    Add deploy picker

---
+++ bin/picker.py
@@ -0,0 +1,27 @@
+import pick
+import subprocess
+from signal import signal, SIGINT
+from sys import exit
+
+
+def handler(signal_received, frame):
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    exit(0)
+
+
+if __name__ == '__main__':
+    # Tell Python to run the handler() function when SIGINT is recieved
+    signal(SIGINT, handler)
+    subprocess.call(['git', 'fetch', 'origin'])
+    tags_cmd = 'git tag -l -n --sort=-v:refname "*.*" | head -5'
+    tags_str = subprocess.check_output(tags_cmd, shell=True)
+    tag_msg, _ = pick.pick(tags_str.splitlines(), 'choose version')
+    env, _ = pick.pick(['stage', 'prod'], 'choose environment')
+    version = tag_msg.split()[0]
+    confirm_msg = 'deploy {} to {}'.format(version, env)
+    _, confirm_res = pick.pick([confirm_msg, 'cancel'], 'confirm')
+    if confirm_res == 0:
+        print(confirm_msg)
+        subprocess.call(["yarn", "deploy", version, env])
+    else:
+        print('cancelled')
