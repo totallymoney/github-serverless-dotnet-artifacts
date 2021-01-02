c9383b08740f43857cdad11dacb72e138229ce86
Blair55 <blair.nicholas@googlemail.com>
2021-01-02 18:40:39 +0000

    Add eslint and lint

--- bin/pick.js
+++ bin/pick.js
@@ -5,26 +5,25 @@ import min from 'minimist'
 import yaml from 'js-yaml'
 import { readFileSync } from 'fs'
 import p from '../lib/prompts.js'
-import tags from '../lib/tags.js'
+import getTags from '../lib/tags.js'
 import deploy from '../lib/deploy.js'
 
-const sls = yaml.safeLoad(readFileSync('serverless.yml'));
+const sls = yaml.safeLoad(readFileSync('serverless.yml'))
 
 const {
-    e: envs = ['stage', 'prod'],
-    c: count = 7
-} = min(process.argv.slice(2));
+  e: envs = ['stage', 'prod'],
+  c: count = 7
+} = min(process.argv.slice(2))
 
 console.clear()
 
-tags(envs, count, sls)
-    .then(tags =>
-        enq.prompt([
-            p.version(tags),
-            p.environments(envs),
-            p.confirm(sls.service),
-        ]))
-    .then(({ version, envs, confirm }) => {
-        if (confirm) deploy(envs, version);
-    })
-    .catch(console.error);
+getTags(envs, count, sls)
+  .then((tags) => enq.prompt([
+    p.version(tags),
+    p.environments(envs),
+    p.confirm(sls.service)
+  ]))
+  .then(({ version, envs: e, confirm }) => {
+    if (confirm) deploy(e, version)
+  })
+  .catch(console.error)
