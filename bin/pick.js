3a7c779fa53d8a0366e98de6c579d75c9d13f7c6
Blair55 <blair.nicholas@googlemail.com>
2021-01-03 00:42:29 +0000

    Ensure envs is an array

--- bin/pick.js
+++ bin/pick.js
@@ -1,24 +1,15 @@
 #!/usr/bin/env node
 
 import enq from 'enquirer'
-import min from 'minimist'
-import yaml from 'js-yaml'
-import { readFileSync } from 'fs'
 import p from '../lib/prompts.js'
 import getTags from '../lib/tags.js'
 import deploy from '../lib/deploy.js'
-
-const sls = yaml.safeLoad(readFileSync('serverless.yml'))
-
-const {
-  e: envs = ['stage', 'prod'],
-  c: count = 7
-} = min(process.argv.slice(2))
+import { envs, count, sls } from '../lib/config.js'
 
 console.clear()
 
 getTags(envs, count, sls)
-  .then((tags) => enq.prompt([
+  .then(tags => enq.prompt([
     p.version(tags),
     p.environments(envs),
     p.confirm(sls.service)
