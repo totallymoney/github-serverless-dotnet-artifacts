038f7db809d94fb5522d3ed76a6a204d13f1f334
Blair55 <blair.nicholas@googlemail.com>
2021-01-10 08:54:13 +0000

    Add more detail to env select
    Disable choices when already deployed to

--- bin/pick.js
+++ bin/pick.js
@@ -4,15 +4,16 @@ import enq from 'enquirer'
 import p from '../lib/prompts.js'
 import getTags from '../lib/tags.js'
 import deploy from '../lib/deploy.js'
-import { envs, count, sls } from '../lib/config.js'
+import { envs, count, lambda, serviceName, functionName }
+  from '../lib/config.js'
 
 console.clear()
 
-getTags(envs, count, sls)
-  .then(tags => enq.prompt([
-    p.version(tags),
-    p.environments(envs),
-    p.confirm(sls.service)
+getTags(envs, count, lambda, functionName)
+  .then(({ tags, decoratedEnvs }) => enq.prompt([
+    p.version(tags, envs),
+    p.environments(decoratedEnvs),
+    p.confirm(serviceName),
   ]))
   .then(({ version, envs: e, confirm }) => {
     if (confirm) deploy(e, version)
