2945d44498ac1e82de648ce251c8b384a7f2e5d5
Blair55 <blair.nicholas@googlemail.com>
2021-01-10 10:39:57 +0000

    Autofocus first selectable version choice
    Add spinner

--- bin/pick.js
+++ bin/pick.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import ora from 'ora'
 import enq from 'enquirer'
 import p from '../lib/prompts.js'
 import getTags from '../lib/tags.js'
@@ -7,11 +8,13 @@ import deploy from '../lib/deploy.js'
 import { envs, count, lambda, serviceName, functionName }
   from '../lib/config.js'
 
+const tags = getTags(envs, lambda, functionName)
 console.clear()
+ora.promise(tags, 'loading')
 
-getTags(envs, count, lambda, functionName)
-  .then(({ tags, decoratedEnvs }) => enq.prompt([
-    p.version(tags, envs),
+tags.then(({ tags, decoratedEnvs }) =>
+  enq.prompt([
+    p.version(tags, envs, count),
     p.environments(decoratedEnvs),
     p.confirm(serviceName),
   ]))
