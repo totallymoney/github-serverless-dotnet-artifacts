22dc99f8f3f47a2f21aa844904dfe1253af2909d
Blair55 <blair.nicholas@googlemail.com>
2021-02-03 14:03:15 +0000

    Add dev deploy script

---
+++ bin/dev-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "$0": usage: quick.sh ENVIRONMENT
+    echo "$0": eg: quick.sh dev
+    exit 1
+fi
+
+ENVIRONMENT=$1
+NOW_ISO=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+export VERSION="0.0.0"
+
+rm -rf "package.zip"
+dotnet lambda package "package.zip" -pl src -c Release
+DEPLOYED_DATE=$NOW_ISO yarn run sls deploy --stage "$ENVIRONMENT" --verbose
