7d8f5a0b91343775af8f47a925b6fa430295bcbc
tombanham <tombanham@users.noreply.github.com>
2022-11-08 16:39:31 +0000

    Optional 2nd argument for project location [improve-187] (#7)
    * improve-187 optional 2nd argument for project location
    
    * improve-187 rename project_location to project_path

--- bin/dev-deploy.sh
+++ bin/dev-deploy.sh
@@ -2,10 +2,17 @@
 
 set -e
 
-if [ $# -ne 1 ]; then
-    echo "$0": usage: quick.sh ENVIRONMENT
-    echo "$0": eg: quick.sh dev
+if [ $# -eq 0 ]; then
+    echo "$0": usage: \'gsda-dev-deploy ENVIRONMENT\' or \'gsda-dev-deploy ENVIRONMENT PROJECT_PATH\'
+    echo "$0": eg: \'gsda-dev-deploy dev\' or \'gsda-dev-deploy dev src\'
     exit 1
+fi
+
+# support an optional 2nd argument for the project path by setting it to a default value if not provided
+if [ $# -eq 1 ]; then
+    PROJECT_PATH="src"
+else
+    PROJECT_PATH=$2
 fi
 
 ENVIRONMENT=$1
@@ -13,5 +20,6 @@ NOW_ISO=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export VERSION="0.0.0"
 
 rm -rf "package.zip"
-dotnet lambda package "package.zip" -pl src -c Release -farch "arm64"
+
+dotnet lambda package "package.zip" -pl "$PROJECT_PATH" -c Release -farch "arm64"
 DEPLOYED_DATE=$NOW_ISO yarn run sls deploy --stage "$ENVIRONMENT" --verbose
