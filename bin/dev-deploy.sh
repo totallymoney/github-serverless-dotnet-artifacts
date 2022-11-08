654dcee7a3ca5413f6cd9eb871ab64e6df6bdf9e
tombanham <tombanham@live.com>
2022-11-08 16:35:59 +0000

    improve-187 rename project_location to project_path

--- bin/dev-deploy.sh
+++ bin/dev-deploy.sh
@@ -3,16 +3,16 @@
 set -e
 
 if [ $# -eq 0 ]; then
-    echo "$0": usage: \'gsda-dev-deploy ENVIRONMENT\' or \'gsda-dev-deploy ENVIRONMENT PROJECT_LOCATION\'
+    echo "$0": usage: \'gsda-dev-deploy ENVIRONMENT\' or \'gsda-dev-deploy ENVIRONMENT PROJECT_PATH\'
     echo "$0": eg: \'gsda-dev-deploy dev\' or \'gsda-dev-deploy dev src\'
     exit 1
 fi
 
-# support an optional 2nd argument for project location by setting it to a default value if not provided
+# support an optional 2nd argument for the project path by setting it to a default value if not provided
 if [ $# -eq 1 ]; then
-    PROJECT_LOCATION="src"
+    PROJECT_PATH="src"
 else
-    PROJECT_LOCATION=$2
+    PROJECT_PATH=$2
 fi
 
 ENVIRONMENT=$1
@@ -21,5 +21,5 @@ export VERSION="0.0.0"
 
 rm -rf "package.zip"
 
-dotnet lambda package "package.zip" -pl "$PROJECT_LOCATION" -c Release -farch "arm64"
+dotnet lambda package "package.zip" -pl "$PROJECT_PATH" -c Release -farch "arm64"
 DEPLOYED_DATE=$NOW_ISO yarn run sls deploy --stage "$ENVIRONMENT" --verbose
