587ae04b8c9baf336a3ad2321237a0559adc5176
Nick Blair <blair.nicholas@gmail.com>
2024-05-21 06:23:30 +0100

    Use gh cli tool for github interactions

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,54 +1,27 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-REQUIREMENTS="jq yarn dotnet curl"
+REQUIREMENTS="gh yarn"
 for i in $REQUIREMENTS; do
-    hash "$i" 2>/dev/null || { echo "$0": I require "$i" but it\'s not installed.; exit 1; }
+  hash "$i" 2>/dev/null || { echo "$0": I require "$i" but it\'s not installed.; exit 1; }
 done
 
-if [ $# -ne 3 ]; then
-    echo "$0": usage: deploy.sh REPOSITORY VERSION ENVIRONMENT
-    echo "$0": eg: deploy.sh mediaingenuity/myrepo 1.2.3456 stage
-    exit 1
+if [ $# -ne 2 ]; then
+  echo "$0": usage: deploy.sh VERSION ENVIRONMENT
+  echo "$0": eg: deploy.sh 1.2.3 stage
+  exit 1
 fi
 
-if [ -z "$GITHUB_OAUTH_TOKEN" ]; then
-    echo "Need to set GITHUB_OAUTH_TOKEN"
-    exit 1
-fi
-
-REPOSITORY=$1
-export VERSION=$2
-ENVIRONMENT=$3
-
+VERSION=$1
+ENVIRONMENT=$2
 NOW_ISO=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-GITHUB_AUTH_HEADER="Authorization: token $GITHUB_OAUTH_TOKEN"
-RELEASES_API="https://api.github.com/repos/$REPOSITORY/releases"
 
-VERSION_DETAILS_IN_JSON=$(curl -s -H "$GITHUB_AUTH_HEADER" "$RELEASES_API/tags/$VERSION")
-ERROR_MESSAGE=$(echo "$VERSION_DETAILS_IN_JSON" | jq ".message")
-
-if [ "$ERROR_MESSAGE" == "\"Bad credentials\"" ]; then
-    echo "Error: Invalid GITHUB_OAUTH_TOKEN"
-    exit 1
-elif [ "$ERROR_MESSAGE" == "\"Not found\"" ]; then
-    echo "Error: Could not find github repository, ensure the GITHUB_OAUTH_TOKEN and the VERSION are correct"
-    exit 1
-elif [ "$ERROR_MESSAGE" != "null" ]; then
-    echo "Error getting github repository: ${ERROR_MESSAGE}"
-    exit 1
-fi
-
-ASSET_ID=$(echo "$VERSION_DETAILS_IN_JSON" | jq ".assets[0].id")
-DEPLOY_DIR="deploy"
-DEPLOY_ZIP="$DEPLOY_DIR.zip"
-
-curl -sL -H "$GITHUB_AUTH_HEADER" -H "Accept: application/octet-stream" "$RELEASES_API/assets/$ASSET_ID" -o "$DEPLOY_ZIP"
-rm -rf $DEPLOY_DIR
-unzip $DEPLOY_ZIP -d $DEPLOY_DIR
-rm -rf $DEPLOY_ZIP
-cd $DEPLOY_DIR
-yarn install -s --no-progress --frozen-lockfile
+rm -rf ./deploy ./deploy.zip
+gh release download "$VERSION" --output deploy.zip
+unzip ./deploy.zip -d ./deploy
+cd ./deploy
+yarn install --silent --no-progress --frozen-lockfile
 DEPLOYED_DATE=$NOW_ISO yarn run sls deploy --stage "$ENVIRONMENT" --verbose
 cd .. || exit
+rm -rf ./deploy ./deploy.zip
--- bin/publish.sh
+++ bin/publish.sh
@@ -1,86 +1,35 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-REQUIREMENTS="jq yarn dotnet curl"
+REQUIREMENTS="gh dotnet"
 for i in $REQUIREMENTS; do
-    hash "$i" 2>/dev/null || { echo "$0": I require "$i" but it\'s not installed.; exit 1; }
+  hash "$i" 2>/dev/null || { echo "$0": I require "$i" but it\'s not installed.; exit 1; }
 done
 
-if [ $# -ne 4 ]; then
-    echo "$0": usage: publish.sh REPOSITORY PROJECT VERSION COMMIT
-    echo "$0": eg: publish.sh mediaingenuity/myrepo src 1.2.3456 master
-    echo "$0": OR
-    echo "$0": eg: publish.sh mediaingenuity/myrepo path/to/project.fsproj 1.2.3456 b7232ef30b17afbe6d6b4703784cd5e69956e104
-    exit 1
+if [ $# -eq 0 ]; then
+  echo "$0": "usage: publish.sh VERSION [ -p project ]" >&2
+  echo "$0": "eg: publish.sh 1.2.3" >&2
+  echo "$0": "eg: publish.sh 1.2.3 -p project/dir" >&2
+  exit 1
 fi
 
-if [ -z "$GITHUB_OAUTH_TOKEN" ]; then
-    echo "Need to set GITHUB_OAUTH_TOKEN"
-    exit 1
-fi
+VERSION=$1
+shift
 
-REPOSITORY=$1
-PROJECT=$2
-VERSION=$3
-COMMIT=$4
-PUBLISH_DIR="publish"
-PUBLISH_ZIP="$VERSION.zip"
+PROJECT=./src
+while getopts "p::" opt; do
+  case $opt in
+    p) PROJECT=${OPTARG} ;;
+    *) ;;
+  esac
+done
 
-rm -rf $PUBLISH_DIR
-mkdir $PUBLISH_DIR
-dotnet lambda package "$PUBLISH_DIR/package.zip" \
-  -pl "$PROJECT" -c Release -farch "arm64" --msbuild-parameters "/p:PublishReadyToRun=true"
-cp serverless.yml "$PUBLISH_DIR/serverless.yml"
-cp package.json "$PUBLISH_DIR/package.json"
-cp yarn.lock "$PUBLISH_DIR/yarn.lock"
-[ -d "./serverless-artifacts" ] && cp -r serverless-artifacts "$PUBLISH_DIR"
-cd $PUBLISH_DIR
-zip -r "$PUBLISH_ZIP" .
-cd .. || exit
-
-LATEST_TAG="$(git describe --tags --abbrev=0)"
-MSGS_SINCE_LATEST_TAG="$(git shortlog "$LATEST_TAG".."$COMMIT" --pretty="%h %s")"
-GITHUB_AUTH_HEADER="Authorization: token $GITHUB_OAUTH_TOKEN"
-
-CREATE_RELEASE_REQUEST=$(
-  jq -n \
-    --arg name "$VERSION" \
-    --arg commit "$COMMIT" \
-    --arg body "$MSGS_SINCE_LATEST_TAG" \
-    '{ tag_name: $name, name: $name, target_commitish: $commit, body: $body }')
-
-echo "Create release request:"
-echo "$CREATE_RELEASE_REQUEST"
-echo "**********************"
-
-CREATE_RELEASE_RESPONSE=$(
-  curl -s -X POST \
-    -H "$GITHUB_AUTH_HEADER" \
-    -d "$CREATE_RELEASE_REQUEST" \
-    "https://api.github.com/repos/$REPOSITORY/releases")
-
-echo "Create release response:"
-echo "$CREATE_RELEASE_RESPONSE"
-echo "**********************"
-
-RELEASE_UPLOAD_URL=$(
-  echo "$CREATE_RELEASE_RESPONSE" |
-    jq ".upload_url" |
-    sed "s/{?name,label}/?name=$PUBLISH_ZIP/" |
-    sed 's/"//g')
-
-echo "Upload url:"
-echo "$RELEASE_UPLOAD_URL"
-echo "**********************"
-
-UPLOAD_RESPONSE=$(
-  curl -s -X POST \
-    -H "$GITHUB_AUTH_HEADER" \
-    -H "Content-Type: application/octet-stream" \
-    --data-binary @"$PUBLISH_DIR/$PUBLISH_ZIP" \
-    "$RELEASE_UPLOAD_URL")
-
-echo "Upload response:"
-echo "$UPLOAD_RESPONSE"
-echo "**********************"
+rm -rf ./publish
+mkdir ./publish
+dotnet lambda package ./publish/package.zip -pl "$PROJECT" -c Release -farch arm64
+cp ./{serverless.yml,package.json,yarn.lock} ./publish
+[ -d "./serverless-artifacts" ] && cp -r ./serverless-artifacts ./publish
+zip -r "./publish/archive.zip" ./publish
+gh release create "$VERSION" "./publish/archive.zip" --generate-notes
+rm -rf ./publish
